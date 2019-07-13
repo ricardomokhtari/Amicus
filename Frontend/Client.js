edaee04478d8882479a4c22330afcfc20f26526f
srdsam <sadsouza9999@gmail.com>
2019-07-13 19:21:56 +0100

    Comment deletion

--- Frontend/Client.js
+++ Frontend/Client.js
@@ -5,12 +5,10 @@ $(document).ready(() => {
 
     const recording = new Recording();
     $('#Start_Recording').on('click', () => {
-        // Starting ASR connection with two parameters. asrConfig is the configuration for the connection. asrCallback is how we handle ASR results
         recording.start();
     });
 
     $('#Stop_Recording').on('click', () => {
-        // Starting ASR connection with two parameters. asrConfig is the configuration for the connection. asrCallback is how we handle ASR results
         recording.stop();
     });
 
