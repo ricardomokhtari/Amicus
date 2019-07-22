53490492b091600f3172a83ca9f316306ca89a28
Ricardo Mokhtari <Ricardo@Ricardos-MBP.home>
2019-07-22 12:44:52 +0100

    Completed audio classification - machine learning model works correctly

--- Backend/audio_classification.py
+++ Backend/audio_classification.py
@@ -13,21 +13,28 @@ import librosa.display
 import os
 import pandas as pd
 
-""" Code below used for loading and visualising .wav files """
+""" 
+Code below used for loading and visualising .wav files 
 
 # data, sampling_rate = librosa.load('train/Train/2022.wav')
 
-#plt.figure(figsize=(12, 4))
-#librosa.display.waveplot(data, sr = sampling_rate)
+# plt.figure(figsize=(12, 4))
+# librosa.display.waveplot(data, sr = sampling_rate)
 
-#train is a dataframe containing item ID and Class
+"""
+
+# train is a dataframe containing item ID and Class
 train = pd.read_csv('train/train.csv')
 
-# the parser function converts .wav files to numpy arrays with associated label
+# lists for storing features (X) and labels (y)
+features = []
+labels = []
+
+# the parser function takes in .wav files and extracts features and labels
 def parser(row):
     
    # function to load files and extract features
-   file_name = os.path.join(os.path.abspath('"""___DATA_DIR_PLUS_ONE___"""'), '"""___DATA_DIR___"""', str(row.ID) + '.wav')
+   file_name = os.path.join(os.path.abspath('train'), 'Train', str(row.ID) + '.wav')
 
    # handle exception to check if there isn't a file which is corrupted
    
@@ -57,25 +64,27 @@ def parser(row):
  
    feature = mfccs
    label = row.Class
+   
+   features.append(feature)
+   labels.append(label)
  
-   return [feature, label]
+   return features, labels
 
 # train.apply(function, axis) applies a function to every row or column of a dataframe
 # axis = 1 means we apply the parser function to every column
 temp = train.apply(parser, axis=1) # executing this line takes a long time
-temp.columns = ['feature','label']
-
-# the code below is for preparing the data and training the model - incomplete
 
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import np_utils
 
-X = np.array(train.ID.tolist())
-y = np.array(train.Class.tolist())
+X = features            # renaming features as X
+X = np.asarray(X)       # input to model has to be numpy array so converting
 
-lb = LabelEncoder()
+y = labels              # y is the dependent variable
 
-y = np_utils.to_categorical(lb.fit_transform(y))
+lb = LabelEncoder()     # y has to be encoded for passing into model
+
+y = np_utils.to_categorical(lb.fit_transform(y))    # y is categorical so it has to be OneHotEncoded
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -102,4 +111,4 @@ model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-model.fit(X, y, batch_size=32, epochs=5, validation_data=(X, y))
+model.fit(X, y, batch_size=32, epochs=20)   # epochs defines the number of iterations of the model
