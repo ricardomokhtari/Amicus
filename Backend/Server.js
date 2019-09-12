4950594802603f9f545004d58caa8cd648060f17
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2019-09-12 14:06:16 +0100

    Updated to work with App.js server request

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,56 +1,43 @@
-var http = require('http');
-var fs = require('fs');
-const WaveFile = require('wavefile');
+const http = require('http')
+const { spawn } = require('child_process')
 
+var server = http.createServer(function(req, res) {
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
 
-// Creating httpServer
-var server = http.createServer();
+    let data = []
 
-console.log("Server Active");
+    req.on('data', function(chunk) {
+        data.push(chunk)
+    });
 
-var audio_array = [];
-var datas = [];
+    req.on('end', function() {
+        console.log("HTTP GET received")
+        data = JSON.parse(data.toString())
+        console.log("JSON: " + data.example)
 
-// Once HTTP is connected do function
-server.on('request', function(req, res) {
-    console.log("-- Request Received --");
-    if (req.method == 'POST') {
-        req.on('data', function(msg) {
-
-            if (msg.toString() == 404) {
-
-                let wav = new WaveFile();
-                datas = new Float32Array(datas);
-                console.log(datas)
-                wav.fromScratch(1, 44100, '32f', datas);
-                fs.writeFileSync('Audio/Test.wav', wav.toBuffer());
-
-            } else {
-                msg = msg.toString();
-
-                data = msg.split(",");
-                for (var i = 0; i < data.length; i++) {
-                    // if ()
-                    datas.push(data[i]);
-                    }
-
-
+        // data = {response: "long string response"}
+        const process = spawn('python', ['./script.py', data.example]);
+        var script_output = []
+        process.stdout.on('data', (data) => {
+            console.log("(python) stdout: " + data.toString())
+            script_output.push(data.toString())
+        });
+    
+        process.stderr.on('data', (data) => {
+            script_output.push(data)
+        });
+        process.on('exit', (code, signal) => {
+            res.writeHead(200, {'x-success': 'OK'});
+            for (let i = 0; i < script_output.length; i++) {
+                res.write(script_output[i])
             }
-        // POST is ended 
-        }).on('error', function(error) {
-            console.log(error.stack);
-        });
+            res.end();
+          });
+    });
 
-        // Need to set 'res' parameters, so HTTP server doesn't freeze. 
-        res.on('error', function(error) {
-            console.log(error);
-        });
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Access-Control-Allow-Origin', "*" );
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-        res.end();
-        
-    };
-});
-
-server.listen(8080);
+}).on('error', (error) => {
+    console.error(error)
+}).listen(8080)
+// server.timeout = 100
