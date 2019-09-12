49faa88a0c325960877e8ddf2fa6a517308fbe17
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2019-09-12 14:09:06 +0100

    Added functionality for running python script and rendering result

--- Frontend/voice-assistant/App.js
+++ Frontend/voice-assistant/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import Logo from './components/logo';
-import TextResponse from './components/textResponse';
-import './App.css';
+import React, { Component } from 'react'
+import Logo from './components/logo'
+import TextResponse from './components/textResponse'
+import './App.css'
 
 class App extends Component {
-  render() { 
+  state = {
+    text: "Hello, press receive data to get response from script.py"
+  }
+
+  constructor(){
+    super()
+    this.getData = this.getData.bind(this)
+  }
+
+  getData() {
+    // create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest()
+
+    // get a callback when the server responds
+    xhr.addEventListener('load', () => {
+      // update the state of the component with the result here
+      console.log(xhr.responseText) // for debugging
+      const text = xhr.responseText
+      this.setState({text: text})
+    })
+    // open the request with the verb and the url
+    xhr.open('POST', 'http://localhost:8080')
+    // send the request
+    xhr.send(JSON.stringify({example: "data"}))
+  }
+
+  render () {
     return (
       <React.Fragment>
-        <main className= "container">
+        <main className = "App">
           <Logo />
-          <TextResponse />
+          <TextResponse text = {this.state.text} />
+          <button onClick = {this.getData} className = "btn btn-secondary btn-m m-2">Receive Data</button>
         </main>
       </React.Fragment>
-    );
+    )
   }
 }
- 
-export default App;
+export default App
