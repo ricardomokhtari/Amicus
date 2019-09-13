44a57cc5ff1aeec49bf3ade04c319e0a4ec46f71
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2019-09-13 18:18:54 +0100

    Added dialogue box for user input (temporary)

--- Frontend/voice-assistant/App.js
+++ Frontend/voice-assistant/App.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react'
 import Logo from './components/logo'
 import TextResponse from './components/textResponse'
 import './App.css'
+import Query from './components/query'
 
 class App extends Component {
   state = {
     // App component has state text which is passed to textResponse via props
-    text: "Hello, my name is Amicus, your virtual assistant. Ask me anything about dementia..."
+    text: "Hello! My name is Amicus, your virtual assistant. Ask me anything about dementia..."
   }
 
   constructor(){
     super()
-    // binding this to getData()
+    // binding this to getData
     this.getData = this.getData.bind(this)
   }
 
   // function that requests data from backend server
   getData() {
+    var input = document.getElementById("data").value;
+
     // create a new XMLHttpRequest
     var xhr = new XMLHttpRequest()
 
@@ -30,7 +33,9 @@ class App extends Component {
     // open the request with the verb and the url
     xhr.open('POST', 'http://localhost:8080')
     // send the request
-    xhr.send(JSON.stringify({user_input: "How many people in the UK have dementia?"}))
+    xhr.send(JSON.stringify({user_input: input}))
+
+    document.getElementById("data").value = ""
   }
 
   render () {
@@ -40,11 +45,13 @@ class App extends Component {
           <div className = "container">
             <Logo />
             <TextResponse text = {this.state.text} />
-            <button onClick = {this.getData} className = "btn btn-secondary btn-m m-2">Send New Request</button>
+            <Query getData = {this.getData}/>
+            <TextResponse text = {"TIP: Type 'bye' at any time to exit."}/>
           </div>
         </main>
       </React.Fragment>
     )
   }
 }
+
 export default App
