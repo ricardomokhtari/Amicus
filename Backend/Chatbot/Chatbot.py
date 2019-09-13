5e873b32c3d30b99f79dfeafd1e42d11b6c8c181
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2019-09-13 14:18:20 +0100

    Adapted for functioning with http request

--- Backend/Chatbot/Chatbot.py
+++ Backend/Chatbot/Chatbot.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import warnings
+import sys
 warnings.filterwarnings('ignore')
 
 import nltk
@@ -64,23 +65,18 @@ def response(user_response):
         robo_response = robo_response+sent_tokens[idx]
         return robo_response
 
-
-flag=True
-print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
-while(flag==True):
-    user_response = input()
-    user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
-            print("ROBO: You are welcome..")
-        else:
-            if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
-            else:
-                print("ROBO: ",end="")
-                print(response(user_response))
-                sent_tokens.remove(user_response)
-    else:
+user_response = sys.argv[1]
+user_response=user_response.lower()
+if(user_response!='bye'):
+    if(user_response=='thanks' or user_response=='thank you' ):
         flag=False
-        print("ROBO: Bye! take care..")
+        print("You are welcome..")
+    else:
+        if(greeting(user_response)!=None):
+            print(greeting(user_response))
+        else:
+            print(response(user_response) + "\nAsk me something else or type 'bye' to exit...")
+            sent_tokens.remove(user_response)
+else:
+    flag=False
+    print("ROBO: Bye! take care..")
